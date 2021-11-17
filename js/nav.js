a5065d996ed4320dd562f4a93260a8953b13b594
Pereyra-Fernando <pereyra.r.fernando@gmail.com>
2021-11-17 12:07:09 -0300

    agrega hasta soluciones

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+const hamburguesa = document.querySelector('#hamburger');
+const menu = document.querySelector('#menu');
+const header = document.querySelector('.header');
+
+
+
+hamburguesa.addEventListener('click', ()=>{ 
+    hamburguesa.classList.toggle('animate');
+    menu.classList.toggle('menu--mostrar');
+    
+})
+
+
